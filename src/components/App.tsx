f56d8c072f778e6e67eb537268887733dca34f9a
John Tucker <john@larkintuckerllc.com>
2020-01-05 16:53:36 -0500

    hello world

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { Text, View } from 'react-native';
+
+const App: FC = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+export default App;
